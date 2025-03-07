f5259e0e04fc7cfbf7ba814730ea946f508cb1c8
4D4M5T0 <adam.stolarski06@gmail.com>
2025-03-07 09:24:48 +0100

    commit

---
+++ test_base.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+
+class TestDatabase(TestCase):
+    def test_create_tables(self):
+        self.fail()
+
+    def test_sprawdzanie(self):
+        self.fail()
+
+    def test_wpisz_uzytkownika(self):
+        self.fail()
+
+    def test_get_uzytkownik_id(self):
+        self.fail()
+
+    def test_wypisz_notatki_uzytkownika(self):
+        self.fail()
+
+    def test_wpisz_notatka(self):
+        self.fail()
+
+    def test_usun_notatka(self):
+        self.fail()
+
+    def test_close(self):
+        self.fail()
---
+++ test_main.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+
+class TestMain(TestCase):
+    def test_rejestracja(self):
+        self.fail()
+
+    def test_log(self):
+        self.fail()
+
+    def test_otwieranie_notatki(self):
+        self.fail()
+
+    def test_dodawanie_notatki(self):
+        self.fail()
+
+    def test_wyswietlanie(self):
+        self.fail()
+
+    def test_wylogowywanie(self):
+        self.fail()
+
+    def test_usuwanie_notatki(self):
+        self.fail()
+
+    def test_okno_logowania(self):
+        self.fail()
